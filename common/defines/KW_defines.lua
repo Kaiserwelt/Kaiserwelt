a669c4b35ba400059bf2b2cae5d20515b1e87668
Nyuhei <nyuhei06@gmail.com>
2024-07-20 23:51:26 +0900

    fix

---
+++ common/defines/KW_defines.lua
@@ -0,0 +1,6 @@
+NDefines.NGame.START_DATE = "1956.1.1.12"
+NDefines.NGame.END_DATE = "1999.1.1.1"
+
+NDefines.NGraphics.POLITICAL_GRID_SMALL_BOX_LIMIT = 8						-- Vanilla is 6
+NDefines.NGraphics.COUNTRY_FLAG_SMALL_TEX_WIDTH = 11						-- Vanilla is 10
+NDefines.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_WIDTH = 11
